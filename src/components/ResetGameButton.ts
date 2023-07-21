3df84234564a4673db0ec601dd7eec03dec09e90
Adam Cross <adamfgcross@gmail.com>
2023-07-21 12:34:50 -0400

    wrap in defineComponent

--- src/components/ResetGameButton.ts
+++ src/components/ResetGameButton.ts
@@ -1,8 +1,9 @@
-export default {
+import { defineComponent } from "vue";
+export default defineComponent({
     name : "reset-game-button",
     methods : {
         reset () {
             this.$emit("event_reset");
         }
     }
-};
+});
